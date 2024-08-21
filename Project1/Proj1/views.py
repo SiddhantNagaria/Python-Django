c5872422e2dc9ba27e2879d8dffb0cbe81da9445
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-22 01:38:52 +0530

    handle models and urls

--- Project1/Proj1/views.py
+++ Project1/Proj1/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import ChaiVariety
 
 # Create your views here.
 
 def all_chai(request):
-    return render(request, 'Proj1/all_chai.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'Proj1/all_chai.html', {'chais':chais})
+
+def chai_detail(request, chai_id):
+  chai = get_object_or_404(ChaiVariety, pk=chai_id)
+  return render(request, 'Proj1/chai_detail.html', {'chai': chai})
--- Project1/Project1/urls.py
+++ Project1/Project1/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -26,4 +28,5 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('chai/',include('Proj1.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
