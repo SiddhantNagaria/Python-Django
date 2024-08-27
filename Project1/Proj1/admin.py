0206d52579ee0e5f1b2b8d4749ae09d3efcf0cea
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-28 00:47:00 +0530

    admin dashboard for relationships

--- Project1/Proj1/admin.py
+++ Project1/Proj1/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
+from .models import ChaiVariety, ChaiReview, Store, ChaiCertificate
 
-from .models import ChaiVariety
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 1
 
-# Register your models here.
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [ChaiReviewInline]
 
-admin.site.register(ChaiVariety)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varieties',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number', 'issued_date', 'valid_until')
+
+admin.site.register(ChaiVariety, ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
