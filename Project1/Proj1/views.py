bcb77087f991c4ccb3be5e7a7826d1957279fd35
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-29 16:58:18 +0530

    django forms

--- Project1/Proj1/views.py
+++ Project1/Proj1/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-
-from .models import ChaiVariety
-
+from .models import ChaiVariety, Store
+from .forms import ChaiVarietyForm
 # Create your views here.
 
 def all_chai(request):
@@ -12,3 +11,15 @@ def all_chai(request):
 def chai_detail(request, chai_id):
   chai = get_object_or_404(ChaiVariety, pk=chai_id)
   return render(request, 'Proj1/chai_detail.html', {'chai': chai})
+
+def chai_store_view(request):
+  stores = None
+  if request.method == 'POST':
+    form = ChaiVarietyForm(request.POST)
+    if form.is_valid():
+      chai_variety = form.cleaned_data['chai_variety']
+      stores = Store.objects.filter(chai_varieties = chai_variety)
+  else:
+    form = ChaiVarietyForm()
+  return render(request,'Proj1/chai_stores.html', {'stores':stores,
+  'form':form}) 
